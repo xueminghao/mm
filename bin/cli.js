d53625becf8441453ec643ddf791088d3e145d2f
薛明浩 <xue_minghao@qq.com>
2019-08-05 12:38:08 +0800

    chore: publish 1.0.8

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ const sender = require('./packages/lib/sender')
 const chalk = require('chalk')
 
 const program = require('commander')
-program.version('1.0.7')
+program.version('1.0.8')
 
 program.command('init')
         .description('Init mm')
